d1b34b3cc1aefbb7d4f6cf36f59ae86ebda33ed1
asraful-07 <asraful0508@gmail.com>
2024-09-08 17:15:53 +0600

    solves problem

---
+++ index3.js
@@ -0,0 +1,14 @@
+// problem 3
+function deletInvlids(array){
+    let numberArray = [];
+    for(let element of array){
+        if(typeof element === 'number' && isNaN (element) === false){
+        numberArray.push(element);
+        }
+    }
+    return numberArray;
+}
+
+const total = deletInvlids[1, null, undefined, 18, -19, NaN, '12', [1, 2], {od: 'lala'}];
+console.log(total);
+console.log(deletInvlids['1', {num: 2}, NaN]);
