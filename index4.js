59f6fb593e87062622860513822ae8a1d9cda062
asraful-07 <asraful0508@gmail.com>
2024-09-08 17:55:46 +0600

    solve problem

---
+++ index4.js
@@ -0,0 +1,16 @@
+// problem 4
+function pasaword(obj){
+    if(obj.name === undefined ||
+         obj.birthYear === undefined || 
+         obj.siteName === undefined || 
+         obj.birthYear.toString().length !== 4){
+            return 'invalid';
+        }
+    let capitalSiteName = obj.siteName[0].toUpperCase() + obj.siteName.slice(1);
+    let pasawordString = capitalSiteName + '#' + obj.name + '@' + obj.birthYear ;
+    return pasawordString;
+}
+
+console.log(pasaword({name: 'hlkdhs', birthYear: 1999, siteName: 'google'}));
+console.log(pasaword({name: 'ikguiy', birthYear: 1999, siteName: 'google'}));
+console.log(pasaword({name: 'toky', birthYear: 2000, siteName: 'facebook'}));
