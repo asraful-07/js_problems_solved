a469e3fd98dc818000311cfa787f56e4e308c13e
asraful-07 <asraful0508@gmail.com>
2025-11-07 10:15:17 +0600

    solve problem

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const arr = [1, 2, 2, 3, 4, 5, 6, 7, 8, 9];
+function countOccurrences(arr, n, x) {
+  let res = 0;
+  for (let i = 0; i < n; i++) {
+    if (x == arr[i]) res++;
+  }
+  return res;
+}
 
-// solve this
-const res = [...new Set(arr)];
-console.log(res);
+arr = [5, 6, 11, 12, 98, 5];
+let n = arr.length;
+let x = 2;
+console.log(countOccurrences(arr, n, x));
