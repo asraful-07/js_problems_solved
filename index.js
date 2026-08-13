bda3b7e0cc3cd72389aa73d310f6f5ea62bc27b5
asraful-07 <asraful0508@gmail.com>
2026-08-13 11:28:31 +0600

    solve problem

--- index.js
+++ index.js
@@ -33,3 +33,16 @@ Promise.resolve().then(() => {
 });
 
 console.log("End");
+
+const sentence = "I am learning Programming to become a programmer";
+
+let str = sentence.split(" ");
+let longest = "";
+
+for (let word of str) {
+  if (word.length > longest.length) {
+    longest = word;
+  }
+}
+
+console.log(longest);
