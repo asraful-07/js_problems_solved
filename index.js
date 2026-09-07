80fd9493c0278ec0a2b7dd6dc7badeea2ef8a9e2
asraful-07 <asraful0508@gmail.com>
2026-09-07 10:18:53 +0600

    problem solve

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const arr = [6, "six", true, false, true, true, false, true, 9, "hello"];
+const arr = [
+  6,
+  "six",
+  true,
+  false,
+  true,
+  true,
+  false,
+  true,
+  10000,
+  46000,
+  9,
+  "hello",
+];
 
 function show(array) {
   if (!Array.isArray(array)) {
