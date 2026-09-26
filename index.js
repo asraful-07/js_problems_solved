fd872ef890c53eddbd897fe59fdaaa8e1f38c79d
asraful-07 <asraful0508@gmail.com>
2026-09-26 09:27:48 +0600

    solve problem

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function countVowels(str) {
-  const vowels = "aeiouAEIOU";
-  let count = 0;
+// function countVowels(str) {
+//   const vowels = "aeiouAEIOU";
+//   let count = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i])) {
-      count++;
-    }
-  }
-  return count;
-}
+//   for (let i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
 const word = countVowels("fruits is health for good");
 console.log(word);
